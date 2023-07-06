d466f1be3b60c12019219c9f3163644ec8405c00
xadabunu <2907xadabunu@student.s19.be>
2023-07-06 02:29:54 +0200

    begnning of implementation for exec function

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: xadabunu <xadabunu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 18:31:55 by xadabunu          #+#    #+#             */
-/*   Updated: 2023/07/04 01:26:12 by xadabunu         ###   ########.fr       */
+/*   Updated: 2023/07/06 02:27:18 by xadabunu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,8 @@ int	check_args(int argc, char **argv)
 
 t_pipex	*free_struct(t_pipex *pipex)
 {
-	if (pipex)
-	{
-		if (pipex->envp)
-			free(pipex->envp);
-		free(pipex);
-	}
+	if (pipex->envp)
+		free(pipex->envp);
 	return (NULL);
 }
 
@@ -80,29 +76,22 @@ void	open_files(t_pipex *p)
 	p->fd[1] = open(p->outfile, O_WRONLY | O_TRUNC);
 }
 
-t_pipex	*create_pipex(char **envp, char **argv, int argc)
+t_pipex	*create_pipex(t_pipex *p, char **envp, char **argv, int argc)
 {
-	t_pipex	*pipex;
-
-	pipex = malloc(sizeof(*pipex));
-	if (pipex)
+	p->here_doc = ft_strncmp(argv[1], "here_doc", 9) == 0;
+	p->envp = parse_path(envp);
+	if (p->envp == NULL)
+		perror("envp error");
+	if (p->here_doc == 1 && here_doc(argv[2], ft_strlen(argv[2]), p) == -1)
 	{
-		pipex->here_doc = ft_strncmp(argv[1], "here_doc", 9) == 0;
-		pipex->envp = parse_path(envp);
-		if (pipex->envp == NULL)
-			perror("envp error");
-		if (pipex->here_doc == 1 && \
-			here_doc(argv[2], ft_strlen(argv[2]), pipex) == -1)
-		{
-			free_struct(pipex);
-			exit (2);
-		}
-		else
-			pipex->infile = argv[1];
-		pipex->outfile = argv[argc - 1];
-		open_files(pipex);
+		free_struct(p);
+		exit (2);
 	}
-	return (pipex);
+	else
+		p->infile = argv[1];
+	p->outfile = argv[argc - 1];
+	p->last_cmd = argc - 2;
+	open_files(p);
 }
 
 void	parse_argv(t_pipex *pipex, char **argv)
@@ -175,13 +164,53 @@ void	exec_last_cmd(t_pipex	*pipex, char *cmd)
 	char		*temp;
 }
 
-void	exec_nth_cmd(t_pipex *pipex, unsigned int n)
+void	dup_fds(int fd_read, int fd_write, t_pipex *pipex)
 {
-	if (pipe(pipex->fd) == -1)
+	int	a;
+	int	b;
+
+	a = dup2(fd_read, STDIN_FILENO);
+	b = dup2(fd_write, STDOUT_FILENO);
+	close(fd_read);
+	close(fd_write);
+	if (a == -1 || b == -1)
+		ft_leave(pipex, EXIT_FAILURE);
+}
+
+void	ft_pipe(t_pipex *pipex)
+{
+	if (pipe(pipex->pipe) == -1)
 	{
 		perror("pipe");
+		pipex->pipe[0] = -1;
+		pipex->pipe[1] = -1;
 	}
-	if (n == 1)
+}
+
+void	exec_cmd(t_pipex *pipex,unsigned int n , int cmd)
+{
+	int	fd_read;
+	int	fd_write;
+	int	id;
+
+	if (cmd == pipex->last_cmd)
+		fd_write = pipex->fd_outfile;
+	else
+	{
+		ft_pipe(pipex);
+		fd_write = pipex->pipe[1];
+	}
+	id = fork();
+	if (id == 0)
+		exec_child(pipex, pipex->args);
+	close(fd_read);
+	close(fd_write);
+	if (id == -1)
+		perror("fork");
+	if (/*last cmd*/)
+	{
+		/*save id => if last cmd failed ?*/
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -190,17 +219,13 @@ int	main(int argc, char **argv, char **envp)
 	int		n;
 
 	if (check_args(argc, argv) == EXIT_FAILURE)
-		exit(2);
-	pipex = create_pipex(envp, argv, argc);
-	if (!pipex)
-		return (2);
+		return (EXIT_FAILURE);
+	create_pipex(pipex, envp, argv, argc);
 	if (ft_strncmp(argv[1], "here_doc", 9) == 0)
 		here_doc(argv[2], ft_strlen(argv[2]), pipex);
-	exec_first_cmd(pipex, argv[1]);
 	n = 1;
 	while (n++ < argc - 3)
 		exec_nth_cmd(pipex, n);
-	exec_last_cmd(pipex, argv[argc - 2]);
 	close(pipex->fd[0]);
 	close(pipex->fd[1]);
 	parse_argv(pipex, argv);
--- pipex.h
+++ pipex.h
@@ -24,10 +24,13 @@ typedef struct s_pipex
 	const char	**envp;
 	char		**args;
 	char		**path;
-	int			fd[2];
+	int			last_cmd;
+	int			pipe[2];
 	int			here_doc;
 	char		*infile;
+	int			fd_infile;
 	char		*outfile;
+	int			fd_outfile;
 }				t_pipex;
 
 char	**split_path(const char *s, char c);
